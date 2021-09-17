237ac9b80cbb941b05ae61581b480eca368ab360
Wesley <wesleyadwebster@gmail.com>
2021-09-16 23:46:31 -0500

    adding files

--- web/src/images/images.js
+++
@@ -1,9 +0,0 @@
-import headshot_Erin from './headshots/erinjerri-headshot.png';
-import headshot_Charity from './headshots/charity-nicolas-headshot.jpeg';
-import headshot_Ian from './headshots/Ian-Gomez-headshot.jpg';
-import headshot_Jamie from './headshots/jamie-cristal-headshot.jpg';
-import headshot_Aldrin from './headshots/Aldrin-headshot.jpg';
-import headshot_Chris from './headshots/chris-de-leon-headshot.jpeg';
-import headshot_Ardees from './headshots/Ardees-Rabang--headshot.jpeg';
-
-export { headshot_Erin, headshot_Charity, headshot_Ian, headshot_Jamie, headshot_Aldrin, headshot_Chris, headshot_Ardees }
