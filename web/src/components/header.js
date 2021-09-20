245a864ad5d5a2e7cee6448d39bd47bbb3184167
Christopher De Leon <cjdeleon062@gmail.com>
2021-09-19 23:41:03 -0400

    add basic svg and header

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,50 +1,74 @@
 import { Link } from "gatsby";
 import React from "react";
 import CTALink from "./CTALink";
+import FasterLogo from "./icon/faster-logo";
 
 const Header = ({ showNav, siteTitle, scrolled, navMenuItems = [], textWhite = true }) => {
-  let headerClass = "fixed w-full z-30 top-0 text-white";
-  headerClass += scrolled ? " bg-white shadow" : "";
+  // let headerClass = "fixed w-full z-30 top-0 text-white";
+  // headerClass += scrolled ? " bg-white shadow" : "";
 
-  let navActionClass =
-    "mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75";
-  navActionClass += !textWhite || !scrolled ? " bg-white text-gray-800" : "";
-  navActionClass += textWhite || scrolled ? " gradient text-white" : "";
+  // let navActionClass =
+  //   "mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75";
+  // navActionClass += !textWhite || !scrolled ? " bg-white text-gray-800" : "";
+  // navActionClass += textWhite || scrolled ? " gradient text-white" : "";
 
-  let navContentClass =
-    "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20";
-  navContentClass += !textWhite || !scrolled ? " lg:bg-transparent bg-gray-100" : "";
-  navContentClass += textWhite || scrolled ? " bg-white" : "";
+  // let navContentClass =
+  //   "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20";
+  // navContentClass += !textWhite || !scrolled ? " lg:bg-transparent bg-gray-100" : "";
+  // navContentClass += textWhite || scrolled ? " bg-white" : "";
 
-  let titleClass = "toggleColour no-underline hover:no-underline font-bold text-2xl lg:text-4xl";
-  titleClass += !textWhite || scrolled ? " text-gray-800" : "";
-  titleClass += textWhite || !scrolled ? " text-white" : "";
+  // let titleClass = "toggleColour no-underline hover:no-underline font-bold text-2xl lg:text-4xl";
+  // titleClass += !textWhite || scrolled ? " text-gray-800" : "";
+  // titleClass += textWhite || !scrolled ? " text-white" : "";
 
+  // return (
+  //   <nav id="header" className={headerClass}>
+  //     <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        // <div className="pl-4 flex items-center">
+        //   <Link id="siteTitle" className={titleClass} to="/">
+        //     {siteTitle}
+        //   </Link>
+        // </div>
+
+  //       {showNav && navMenuItems && (
+  //         <div className={navContentClass} id="nav-content">
+  //           <ul className="list-reset lg:flex justify-end flex-1 items-center">
+  //             {navMenuItems.map((i, index) => (
+  //               <li key={`cta-${index}`} className="mr-3">
+  //                 <CTALink {...i} buttonActionClass={navActionClass} />
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //       )}
+  //     </div>
+
+  //     <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+  //   </nav>
+  // );
+// bg color of nav: rgba(60, 100, 177, 1)
   return (
-    <nav id="header" className={headerClass}>
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+  <nav className="w-full z-100 top-0 text-gray-900 bg-blue-100 px-4 py-6">
+      <div className="w-full container mx-auto flex flex-wrap items-center justify-between">
         <div className="pl-4 flex items-center">
-          <Link id="siteTitle" className={titleClass} to="/">
-            {siteTitle}
+          <Link id="siteTitle" to="/" style={{width: "140px"}}>
+            <FasterLogo svgTitle={siteTitle}/>
           </Link>
         </div>
-
         {showNav && navMenuItems && (
-          <div className={navContentClass} id="nav-content">
-            <ul className="list-reset lg:flex justify-end flex-1 items-center">
-              {navMenuItems.map((i, index) => (
+          <div className="mt-2 text-black z-20">
+            <ul className="flex">
+              {navMenuItems.map((menuItem, index) => (
                 <li key={`cta-${index}`} className="mr-3">
-                  <CTALink {...i} buttonActionClass={navActionClass} />
+                  <CTALink {...menuItem} buttonActionClass="uppercase font-bold text-base"></CTALink>
                 </li>
               ))}
             </ul>
           </div>
         )}
       </div>
-
-      <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
     </nav>
-  );
+  )
 };
 
 export default Header;
