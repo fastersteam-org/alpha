8c8bbfeb573c40f09667d8d8f4f7bc256d5a69c0
Wesley <wesleyadwebster@gmail.com>
2021-09-25 17:23:12 -0500

    progress

--- web/src/components/SpeakerProfile.js
+++ web/src/components/SpeakerProfile.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import * as classes from './SpeakerProfile.module.css';
 
-const { speakerContainer, speakerInformationFlex } = classes;
+const { speakerContainer, speakerInformationFlex, speakerStats } = classes;
 
+// TODO: Needs a parent component, this component will be the one to get iterated over and information passed via props
+// TODO: Mobile first, refactor CSS so that desktop is using media query.
 const SpeakerProfile = (props) => {
   return (
     <section className={speakerContainer}>
@@ -15,13 +17,19 @@ const SpeakerProfile = (props) => {
           Lorem ipsum
         </p>
       </div>
-      <div>
-        <h2>Role</h2>
-        <p>Role Here</p>
-        <h2>Companies</h2>
-        <p>Company Here</p>
-        <h2>Education</h2>
-        <p>Education Here</p>
+      <div className={speakerStats}>
+        <div>
+          <h2>Role</h2>
+          <p>Role Here</p>
+        </div>
+        <div>
+          <h2>Companies</h2>
+          <p>Company Here</p>
+        </div>
+        <div>
+          <h2>Education</h2>
+          <p>Education Here</p>
+        </div>
       </div>
     </section>
   );
