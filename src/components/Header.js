cd59cd4408ce5640e3fad547baf73a97a6e177f9
Christopher De Leon <cjdeleon062@gmail.com>
2022-03-23 17:04:51 -0400

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const links = [
+    'home',
+    'about',
+    'blog',
+];
+
+const Header = () => (
+    <div>
+        This is the header div.
+    </div>
+);
+
+Header.displayName = 'Header';
+export default Header;
