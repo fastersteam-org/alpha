65522aa0854c2b7dc61866cd28e9a3e39291c415
FasterSteam <17349829+fastersteam@users.noreply.github.com>
2022-03-24 00:04:55 -0400

    Enhance/eslint prettier (#1)
    * add header component
    
    * update and add tailwind
    
    * basic link rendering
    
    * add ESLint and Prettier configuration with precommit hooks
    
    * more prettier rewrites
    
    * update prettier configuration
    
    * add hooks eslint rules
    
    * add codeowners
    
    * change workflow to just do one action on push
    
    * update script
    
    Co-authored-by: Christopher De Leon <cjdeleon062@gmail.com>

--- src/templates/pokemon.js
+++ src/templates/pokemon.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Pokemon = ({ pageContext: { pokemon } }) => {
     return (
         <main>
             <h1>{pokemon.name}</h1>
         </main>
-    )
-}
+    );
+};
+
+Pokemon.propTypes = {
+    pageContext: PropTypes.shape({
+        pokemon: PropTypes.shape({
+            name: PropTypes.string,
+        }),
+    }),
+};
 
 export default Pokemon;
