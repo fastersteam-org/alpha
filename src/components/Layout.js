85aa72af66cbe662fe1bffd0a1d78930bf43bc59
Christopher De Leon <chrisdeleon.dev@gmail.com>
2022-06-22 23:01:29 -0400

    add tailwind to layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import Footer from './Footer';
 
+import './layout.scss';
+
 const Layout = ({ children }) => (
     <>
         <Header />
