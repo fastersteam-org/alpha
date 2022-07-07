85be49ee66df24560c1538acac59609dff859815
mxayon <mxnkpl@gmail.com>
2022-07-07 16:36:46 -0700

    appends layout and links css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import * as React from 'react';
+import Layout from '../components/Layout';
 
 // markup
 const IndexPage = () => {
-    return (
-        <main>
-            <div> Hello World! </div>
-            <button className="py-4 px-4 bg-blue-600">Test</button>
-        </main>
-    );
+    return <Layout></Layout>;
 };
 
 export default IndexPage;
