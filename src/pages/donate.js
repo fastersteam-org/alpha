266c2f1a73e9cf92b486457a6cc8eb453cec7256
mxayon <mxnkpl@gmail.com>
2022-08-03 17:50:18 -0700

    sets up donate page

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,37 +1,12 @@
 import * as React from 'react';
+import Layout from '../components/Layout';
+import Donate from '../components/Donate';
 
 const DonatePage = () => {
     return (
-        <main className="py-5 px-10">
-            <div>
-                <form
-                    action="https://www.paypal.com/donate"
-                    method="post"
-                    target="_top"
-                >
-                    <input
-                        type="hidden"
-                        name="hosted_button_id"
-                        value="9ZKVGFY9MKEWC"
-                    />
-                    <input
-                        type="image"
-                        src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-                        border="0"
-                        name="submit"
-                        title="PayPal - The safer, easier way to pay online!"
-                        alt="Donate with PayPal button"
-                    />
-                    <img
-                        alt=""
-                        border="0"
-                        src="https://www.paypal.com/en_US/i/scr/pixel.gif"
-                        width="1"
-                        height="1"
-                    />
-                </form>
-            </div>
-        </main>
+        <Layout>
+            <Donate />
+        </Layout>
     );
 };
 
