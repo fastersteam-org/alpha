058042991b81d39e3e6a0d5964e903419765e5fe
Christopher De Leon <chrisdeleon.dev@gmail.com>
2022-08-20 01:12:24 -0400

    update some spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,17 @@ const IndexPage = () => {
         <Layout>
             {/* Header Secton */}
             <div className="max-w-7xl mx-auto pt-8 px-4 sm:px-6 lg:px-20 text-center">
-                <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl lg:text-4xl uppercase">
+                <h2 className="text-xl font-bold text-gray-900 sm:text-2xl lg:text-4xl uppercase">
                     Welcome to Faster
                 </h2>
-                <p className="mt-2.5 sm:mt-3 lg:mt-4 text-2xl text-gray-900 sm:text-3xl lg:text-4xl">
+                <p className="mt-2.5 sm:mt-3 lg:mt-4 text-xl text-gray-900 sm:text-2xl lg:text-4xl">
                     FILIPINX AMERICANS IN SCIENCE TECHNOLOGY ENGINEERING ARTS
                     AND MATHEMATICS
                 </p>
             </div>
 
             {/* main image Section */}
-            <div className="max-w-7xl pt-12 sm:pt-16 lg:pt-20 w-screen mx-auto">
+            <div className="max-w-7xl pt-8 sm:pt-10 lg:pt-14 w-screen mx-auto">
                 <div className="w-full mx-auto relative pb-[56.25%] bg-indigo-600">
                     <img
                         className="h-full w-full absolute"
@@ -27,19 +27,19 @@ const IndexPage = () => {
             </div>
 
             {/* Landing Content Header */}
-            <div className="max-w-7xl mx-auto pt-12 sm:pt-16 lg:pt-20 px-4 sm:px-6 lg:px-20">
-                <h2 className="text-3xl font-bold text-faster-green sm:text-4xl lg:text-5xl">
+            <div className="max-w-7xl mx-auto pt-8 sm:pt-10 lg:pt-14 px-4 sm:px-6 lg:px-20">
+                <h2 className="text-xl font-bold text-faster-green sm:text-2xl lg:text-4xl">
                     We create, connect, and cultivate a movement of Filipinx
                     Americans in the U.S. tech ecosystem.
                 </h2>
             </div>
 
             {/* About us Section */}
-            <div className="max-w-7xl mx-auto pt-12 sm:pt-16 lg:pt-20 px-4 sm:px-6 lg:px-20">
-                <h3 className="text-lg font-bold text-faster-green sm:text-xl lg:text-2xl">
+            <div className="max-w-7xl mx-auto pt-8 sm:pt-10 lg:pt-14 px-4 sm:px-6 lg:px-20">
+                <h3 className="text-base font-bold text-faster-green sm:text-lg lg:text-xl">
                     About Us
                 </h3>
-                <p className="text-lg sm:text-xl lg:text-2xl">
+                <p className="text-base sm:text-lg lg:text-xl">
                     Established in 2015, FASTER is the only multi-stakeholder
                     national 501(c)(3) non-partisan non-profit organization
                     dedicated to advancing excellence, equality, and justice
@@ -49,11 +49,11 @@ const IndexPage = () => {
             </div>
 
             {/* Our Mission */}
-            <div className="max-w-7xl mx-auto pt-6 sm:pt-8 lg:pt-12 pb-20 px-4 sm:px-6 lg:px-20">
-                <h3 className="text-lg font-bold text-faster-green sm:text-xl lg:text-2xl">
+            <div className="max-w-7xl mx-auto pt-6 sm:pt-8 lg:pt-10 pb-20 px-4 sm:px-6 lg:px-20">
+                <h3 className="text-base font-bold text-faster-green sm:text-lg lg:text-xl">
                     Our Mission
                 </h3>
-                <p className="text-lg sm:text-xl lg:text-2xl">
+                <p className="text-base sm:text-lg lg:text-xl">
                     We create, connect, cultivate a multigenerational movement
                     of Filipinx Americans in the Science Technology Engineering
                     Arts and Mathematics working in the science and technology
