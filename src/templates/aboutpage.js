776347d64a2bb0dc7935aec77e378bbd9f880282
Christopher De Leon <chrisdeleon.dev@gmail.com>
2022-09-01 13:41:07 -0400

    create aboout page

--- src/templates/aboutpage.js
+++ src/templates/aboutpage.js
@@ -1,62 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { GatsbyImage } from 'gatsby-plugin-image';
-// import { BLOCKS } from '@contentful/rich-text-types';
-// import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import Layout from '../components/Layout';
-
-// Render logic for homepage content model blocks
-// const richTextOptions = {
-//     renderNode: {
-//         [BLOCKS.HEADING_3]: (node, children) => (
-//             <h3 className="pt-8 sm:pt-10 lg:pt-14 text-base font-bold text-faster-green sm:text-lg lg:text-xl">
-//                 {children}
-//             </h3>
-//         ),
-//         [BLOCKS.PARAGRAPH]: (node, children) => (
-//             <p className="text-base sm:text-lg lg:text-xl">{children}</p>
-//         ),
-//     },
-// };
 
 const AboutPage = ({ pageContext: { aboutPageData } }) => (
     <Layout>
         <div className="max-w-7xl mx-auto pt-8 px-4 sm:px-6 lg:px-20 text-center">
-            <h2 className="text-xl font-bold text-gray-900 sm:text-2xl lg:text-4xl uppercase"></h2>
-            <p className="mt-2.5 sm:mt-3 lg:mt-4 text-xl text-gray-900 sm:text-2xl lg:text-4xl"></p>
+            <h2 className="text-xl font-bold text-gray-900 sm:text-2xl lg:text-4xl uppercase">
+                FASTER LEADERSHIP TEAM
+            </h2>
+            <p className="mt-2.5 sm:mt-3 lg:mt-4 text-lg text-gray-900 sm:text-xl lg:text-3xl font-medium">
+                FASTER National Board of Directors
+            </p>
         </div>
 
         {/* main image Section */}
         <div className="max-w-7xl pt-8 sm:pt-10 lg:pt-14 w-screen mx-auto">
-            <div className="w-full mx-auto relative pb-[56.25%] bg-indigo-600">
+            <div className="w-full mx-auto">
                 <GatsbyImage
-                    className="h-full w-full absolute top-0 left-0"
                     image={aboutPageData.heroImage.gatsbyImageData}
+                    alt=""
                 />
             </div>
         </div>
 
-        {/* Landing Content Header */}
-        <div className="max-w-7xl mx-auto pt-8 sm:pt-10 lg:pt-14 px-4 sm:px-6 lg:px-20">
-            <h2 className="text-xl font-bold text-faster-green sm:text-2xl lg:text-4xl"></h2>
+        {/* Team Direction and Goals */}
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20 pb-20">
+            <p className="text-base sm:text-lg lg:text-xl mb-8">
+                FASTER operates under the leadership and direction of our
+                National Board of Directors, Advisors, Committee Members, and in
+                partnership with select community partners at the local, state,
+                and national level to continue to address the dynamic needs of
+                the Filipinx American community in the science and technology
+                ecosystem. New leadership will be filled on an ongoing basis as
+                our organization scales and grows.
+            </p>
+            <p className="text-base sm:text-lg lg:text-xl">
+                Learn more about our FASTER National Board of Directors,
+                Advisers, and Committee Members (Tech, Creative).
+            </p>
         </div>
-
-        {/* About us Section */}
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20 pb-20"></div>
     </Layout>
 );
 
 AboutPage.propTypes = {
     pageContext: PropTypes.shape({
         aboutPageData: PropTypes.shape({
-            header: PropTypes.string.isRequired,
-            contentHeader: PropTypes.string.isRequired,
-            subheader: PropTypes.string.isRequired,
             heroImage: PropTypes.shape({
                 gatsbyImageData: PropTypes.shape({}),
-            }),
-            subsections: PropTypes.shape({
-                raw: PropTypes.string.isRequired,
             }),
         }),
     }),
