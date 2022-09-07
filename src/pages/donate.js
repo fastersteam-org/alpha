1df65e6f4f44c89e17efbff319a2e0b995319f42
mxayon <mxnkpl@gmail.com>
2022-09-06 21:47:22 -0700

    spacing and bullet fix

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -8,31 +8,31 @@ const DonatePage = () => {
                 <div className="container mx-auto px-5 py-5 sm:px-6 lg:px-8">
                     <div className="mx-auto bg-gray-50 px-14">
                         <h1 className="pb-10 pt-8">Your Donation</h1>
-                        <p className="pb-10">
+                        <p className="pb-5">
                             Thank you for your time and please consider making a
                             donation to FASTER here.
                         </p>
-                        <p className="pb-10">
+                        <p className="pb-5">
                             All funds will be used cover basic operational costs
                             of our majority volunteer organization, further
                             develop our key signature programs and major
                             executive initiatives.
                         </p>
-                        <p className="pb-10">
+                        <p className="pb-5">
                             Your investment in FASTER helps keep our community
                             events and initiatives running for our FASTER
                             components -- funds go directly back into the FASTER
                             and broader Filipinx American community. Your
                             donation supports the following expenses:
                         </p>
-                        <p className="pb-10">
-                            <ul>
-                                Recurring sofftware and platform fees we use to
+                        <p className="pb-3">
+                            <li>
+                                Recurring software and platform fees we use to
                                 host our membership resources including Zoom,
                                 online community database and small business and
                                 high tech company directory.
-                            </ul>
-                            <ul>
+                            </li>
+                            <li>
                                 Supporting part time staff dedicated to FASTER
                                 Education (education component for K-12 and
                                 undergraduate students and recent graduates
@@ -41,17 +41,17 @@ const DonatePage = () => {
                                 Entrepreneurship (innovation component serving
                                 Filipinx American high tech startup founders and
                                 investors).
-                            </ul>
-                            <ul>
+                            </li>
+                            <li>
                                 Lowering costs for community members with little
                                 to no financial accessibility
-                            </ul>
-                            <ul>
+                            </li>
+                            <li>
                                 Providing monetary honorariums to speakers,
                                 teachers, practitioners, and other organizations
                                 that teach, inspire, and support you and the
                                 rest of the community.
-                            </ul>
+                            </li>
                         </p>
                         <div className="mx-auto py-8 text-center">
                             <form
