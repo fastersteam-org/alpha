effc6c2b99451894eeb76cd3a6d37d3098424244
Christopher De Leon <chrisdeleon.dev@gmail.com>
2022-09-06 20:15:38 -0400

    fix redirects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const navigation = {
     join: {
         name: 'Join',
         items: [
-            { name: 'FASTER Education', href: '/join' },
-            { name: 'FASTER PROS', href: '/join' },
-            { name: 'FASTER FRESH', href: '/join' },
+            { name: 'FASTER Education', href: '/fastercon' },
+            { name: 'FASTER PROS', href: '/fastercon' },
+            { name: 'FASTER FRESH', href: '/fastercon' },
         ],
     },
     archive: {
