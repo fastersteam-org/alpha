c42deeabd5c27a2382c2ff8d5cfdc3a148084dac
mxayon <19865986+mxayon@users.noreply.github.com>
2022-11-08 20:39:06 -0800

    queries all products and creates unique page with handle field

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,3 +59,47 @@ module.exports = {
         },
     ],
 };
+
+const path = require(`path`);
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    // Query for all products in Shopify
+    const result = await graphql(`
+        query MyQuery {
+            allShopifyProduct {
+                edges {
+                    node {
+                        title
+                        handle
+                        variants {
+                            shopifyId
+                            image {
+                                src
+                            }
+                        }
+                        priceRangeV2 {
+                            maxVariantPrice {
+                                amount
+                            }
+                        }
+                        description
+                    }
+                }
+            }
+        }
+    `);
+
+    // Iterate over all products and create a new page using a template
+    // The product "handle" is generated automatically by Shopify
+    result.data.allShopifyProduct.edges.forEach(({ node }) => {
+        createPage({
+            path: `/products/${node.handle}`,
+            component: path.resolve(`./src/templates/product.js`),
+            context: {
+                product: node,
+            },
+        });
+    });
+};
