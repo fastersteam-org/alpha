2966b3e9447bbf46cba2df11f1b759756337decf
mxayon <19865986+mxayon@users.noreply.github.com>
2022-11-21 14:14:01 -0800

    creates template for each product page, displays title and description

---
+++ src/templates/product.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const ProductTemplate = ({ pageContext }) => {
+    const { product } = pageContext;
+    return (
+        <div>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+        </div>
+    );
+};
+
+export default ProductTemplate;
