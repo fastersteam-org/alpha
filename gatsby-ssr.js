0bc9225e94c3b70aba01368bfa6bf25833f2dea2
mxayon <19865986+mxayon@users.noreply.github.com>
2022-11-21 21:11:09 -0800

    adds CombinedProvider to access across shop

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,2 @@
-import '@fontsource/montserrat';
-import '@fontsource/montserrat/700.css';
 import CombinedProvider from './src/context/CombinedProvider';
-
 export const wrapRootElement = CombinedProvider;
